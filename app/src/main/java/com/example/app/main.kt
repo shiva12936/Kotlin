f193532721bfa56b3632af6a301a85b2f0c08a84
shiva12936 <shivanagendra12936@gmail.com>
2021-05-14 22:41:04 +0530

    When

--- app/src/main/java/com/example/app/main.kt
+++ app/src/main/java/com/example/app/main.kt
@@ -1,17 +1,4 @@
 package com.example.app
 fun main(){
-    var n = 11
-    var res: String = if(n>10) "hello" else "world"
-
-    res = when(n){
-        11 -> {
-            "hello\n"
-            "abc"
-        }
-        in 2..12 -> "world"
-        11,12,13 -> "Test"
-        else -> "no data"
-    }
-
-    print(res)
+   print("hello world")
 }
